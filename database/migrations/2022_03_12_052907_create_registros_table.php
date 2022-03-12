53cfed5a4df4414dcf475c1361937138f4915f7b
RaulHernandez14 <hdezraul6@gmail.com>
2022-03-12 11:49:47 -0600

    registros

---
+++ database/migrations/2022_03_12_052907_create_registros_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo', 5);
+            $table->string('asentamiento', 100);
+            $table->string('tipo_asenta', 100);
+            $table->string('municipio', 100);
+            $table->string('estado', 100);
+            $table->string('ciudad', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros');
+    }
+};
